64f34ffe96a8d76da10e2c3a1cb874f64403e3d0
Atthapon Korkaew <atthapon@onedaycat.com>
2018-12-04 16:03:21 +0700

    Add error helper and mapper

---
+++ errormapperlib/src/main/java/com/atthapon/errormapperlib/ErrorMapperImpl.kt
@@ -0,0 +1,15 @@
+package com.atthapon.errormapperlib
+
+class ErrorMapperImpl(val errorCode: HashMap<String, ErrorFactory>) : ErrorMapper {
+    override fun <T> map(error: T, params: String?): Error {
+        if (error as? Error != null) {
+            val errorFactory = errorCode[error.code]
+            if (errorFactory != null) {
+                return errorFactory.invoke(params, error.cause, error.code)
+            } else {
+                throw IllegalArgumentException("Not found error factory with code ${error.code}")
+            }
+        }
+        throw TypeCastException("Can not cast $error to Error type.")
+    }
+}
