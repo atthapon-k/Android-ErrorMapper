ed0fbcc73edeff229e52fa800997a527a836b6fe
Atthapon Korkaew <atthapon@onedaycat.com>
2018-12-21 11:52:39 +0700

    Update library name

---
+++ erdrorlib/src/main/java/com/atthapon/erdrorlib/BaseError.kt
@@ -0,0 +1,6 @@
+package com.atthapon.erdrorlib
+
+open class BaseError(open var code: String) : Exception() {
+    override var cause: Throwable? = null
+    override var message: String? = null
+}
---
+++ erdrorlib/src/main/java/com/atthapon/erdrorlib/ErrorMapperImpl.kt
@@ -0,0 +1,12 @@
+package com.atthapon.erdrorlib
+
+class ErrorMapperImpl(private val errorCode: HashMap<String, ErrorFactory?>) : ErrorMapper {
+    override fun <T> map(error: T, params: String?): Error {
+        if (error as? BaseError != null) {
+            val errorFactory = errorCode[error.code]
+            return errorFactory?.invoke(params, error.cause, error.code)
+                ?: ErrorMapperHelper.unknown(ErrorType.UNKNOWN_ERROR.value, error.message).invoke(params, error.cause, error.code)
+        }
+        throw TypeCastException("Can not cast $error to Error type.")
+    }
+}
