e0768911897010d5fe27ca762bc6f283ab1951cb
atthapon-k <polcpekusrc@gmail.com>
2019-02-25 05:44:39 +0700

    Rename error class

--- library/src/main/java/com/atthapon/library/ErrorMapper.kt
+++ library/src/main/java/com/atthapon/library/ErrorMapper.kt
@@ -1,5 +1,5 @@
 package com.atthapon.library
 
 interface ErrorMapper {
-    fun <T> map(error: T, params: String? = null): Error
+    fun <T> map(error: T, params: String? = null): HomunculiError
 }
---
+++ library/src/main/java/com/atthapon/library/HomunculiErrorFactory.kt
@@ -0,0 +1,70 @@
+package com.atthapon.library
+
+typealias ErrorFactory = (params: String?, cause: Throwable?, causeCode: String?) -> HomunculiError
+
+object HomunculiErrorFactory {
+
+    fun customError(type: String, code: String, message: String): ErrorFactory {
+        return { params: String?, cause: Throwable?, causeCode: String? ->
+            HomunculiError(type, code).build(message, params, cause, causeCode)
+        }
+    }
+
+    fun badRequest(code: String, message: String): ErrorFactory {
+        return { params: String?, cause: Throwable?, causeCode: String? ->
+            HomunculiError(ErrorType.BAD_REQUEST.value, code).build(message, params, cause, causeCode)
+        }
+    }
+
+    fun unauthorized(code: String, message: String): ErrorFactory {
+        return { params: String?, cause: Throwable?, causeCode: String? ->
+            HomunculiError(ErrorType.UNAUTHORIZED.value, code)
+                .build(message, params, cause, causeCode)
+        }
+    }
+
+    fun unavailable(code: String, message: String): ErrorFactory {
+        return { params: String?, cause: Throwable?, causeCode: String? ->
+            HomunculiError(ErrorType.UNAVAILABLE.value, code).build(message, params, cause, causeCode)
+        }
+    }
+
+    fun notFound(code: String, message: String): ErrorFactory {
+        return { params: String?, cause: Throwable?, causeCode: String? ->
+            HomunculiError(ErrorType.NOT_FOUND.value, code).build(message, params, cause, causeCode)
+        }
+    }
+
+    fun internalError(code: String, message: String): ErrorFactory {
+        return { params: String?, cause: Throwable?, causeCode: String? ->
+            HomunculiError(ErrorType.INTERNAL_ERROR.value, code)
+                .build(message, params, cause, causeCode)
+        }
+    }
+
+    fun timeout(code: String, message: String): ErrorFactory {
+        return { params: String?, cause: Throwable?, causeCode: String? ->
+            HomunculiError(ErrorType.TIMEOUT.value, code).build(message, params, cause, causeCode)
+        }
+    }
+
+    fun notImplement(code: String, message: String): ErrorFactory {
+        return { params: String?, cause: Throwable?, causeCode: String? ->
+            HomunculiError(ErrorType.NOT_IMPLEMENT.value, code)
+                .build(message, params, cause, causeCode)
+        }
+    }
+
+    fun forbidden(code: String, message: String): ErrorFactory {
+        return { params: String?, cause: Throwable?, causeCode: String? ->
+            HomunculiError(ErrorType.FORBIDDEN.value, code).build(message, params, cause, causeCode)
+        }
+    }
+
+    fun unknown(code: String, message: String?): ErrorFactory {
+        return { params: String?, cause: Throwable?, causeCode: String? ->
+            HomunculiError(ErrorType.UNKNOWN_ERROR.value, code)
+                .build(message, params, cause, causeCode)
+        }
+    }
+}
